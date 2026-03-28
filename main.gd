f4a00da6fb71f100fb033c6aa5baf300ba872ee0
Santiago Gauto <73668218+Santiago6666@users.noreply.github.com>
2026-03-27 23:36:22 -0300

    Pantalla frontal
    sexta etapa del tutorial 2d, se crea el hud y se enlaza con main para crear un juego jugable

--- main.gd
+++ main.gd
@@ -4,7 +4,7 @@ extends Node
 var score
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	new_game()
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -14,11 +14,15 @@ func _process(delta: float) -> void:
 func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	$HUD.show_game_over()
 
 func new_game():
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
+	get_tree().call_group("mobs", "queue_free")
 
 func _on_mob_timer_timeout():
 	
@@ -42,6 +46,7 @@ func _on_mob_timer_timeout():
 
 func _on_score_timer_timeout():
 	score += 1
+	$HUD.update_score(score)
 
 func _on_start_timer_timeout():
 	$MobTimer.start()
